140907b73c287c532b2ee7422301b49f228c146a
Amhar Rishan <rishanm@mcmaster.ca>
2023-11-30 14:20:14 -0500

    2b-1

--- Player.h
+++ Player.h
@@ -12,7 +12,7 @@ class Player
     // Only some sample members are included here
 
     // You will include more data members and member functions to complete your design.
-
+    
     public:
         enum Dir {UP, DOWN, LEFT, RIGHT, STOP};  // This is the direction state
 
@@ -29,6 +29,9 @@ class Player
 
         // Need a reference to the Main Game Mechanisms
         GameMechs* mainGameMechsRef;
+        
+
+    
 };
 
 #endif
